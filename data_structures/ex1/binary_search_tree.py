4cf2eb7a3c6fc362245bca2ebb8f17cff21ebb8e
Jonathan T. Miles <jonathan.t.miles@gmail.com>
2018-11-16 10:09:30 -0800

    implements depth_first_search on the BST. tests fail because returned with extra set of brackets

--- data_structures/ex1/binary_search_tree.py
+++ data_structures/ex1/binary_search_tree.py
@@ -5,7 +5,38 @@ class BinarySearchTree:
     self.right = None
 
   def depth_first_for_each(self, cb):
-    pass    
+    # initial setup
+    nodes = []
+    values = []
+    current_node = self
+    nodes.append(current_node)
+    values.append(current_node.value)
+    while len(nodes) > 0:
+        # each time there's a left node to explore
+        while current_node != None:
+            nodes.append(current_node.left)
+            if current_node.left:
+                values.append(current_node.left.value)
+                print(current_node.value)
+            current_node = current_node.left
+        # when the left side is exhausted, back up by one and check the right
+        while current_node == None:
+            if nodes[-1] == None:
+                nodes.pop(-1)
+            if len(nodes) == 0:
+                break
+            print(len(nodes))
+            current_node = nodes[-1]
+            print(current_node)
+            print(nodes)
+            # since we pop each time there's a right fork, we trend towards empty
+            nodes.pop(-1)
+            nodes.append(current_node.right)
+            if current_node.right:
+                values.append(current_node.right.value)
+                print(current_node.value)
+            current_node = current_node.right
+    return cb(values.copy())
 
   def breadth_first_for_each(self, cb):
     pass
